dcb78c0daf411571528114644a36f01a4e21c350
AlexWMF <alexxwmf@gmail.com>
2016-08-21 18:02:32 +0300

    [Fix] Now the '0x' prefix is handled successfully

--- trash_jump.py
+++ trash_jump.py
@@ -26,6 +26,7 @@ __author__ = 'AlexWMF'
 
 import idaapi
 import re
+import traceback
 
 
 class trash_jump_plugin_t(idaapi.plugin_t):
@@ -35,18 +36,25 @@ class trash_jump_plugin_t(idaapi.plugin_t):
     help = ""
     wanted_name = "Trash Jump"
     wanted_hotkey = "G"
-    _RE_ADDR = re.compile(r'(\b[0-9a-f]+\b)', re.I | re.U | re.M)
+    _RE_ADDR = re.compile(r'\b(0x)?([0-9a-f]+)\b', re.I | re.U | re.M)
 
     def init(self):
         return idaapi.PLUGIN_OK
 
     @classmethod
     def parse(cls, s):
-        try:
-            s = s.strip()
-            return [long(addr, 16) for addr in cls._RE_ADDR.findall(s)]
-        except:
-            pass
+        rv = list()
+        for _, addr in cls._RE_ADDR.findall(s.strip()):
+            if not addr:
+                continue
+            try:
+                if '0x' not in addr.lower():
+                    addr = '0x' + addr
+                rv.append(long(addr, 16))
+            except:
+                pass
+                print 'err: %s' % traceback.format_exc()
+        return rv
 
     def run(self, arg):
         try:
@@ -65,7 +73,7 @@ class trash_jump_plugin_t(idaapi.plugin_t):
 
             idaapi.msg('TrashJump: address not found. Parsed: %r\n' % [hex(ea) for ea in eas])
         except:
-            idaapi.msg('TrashJump: address not found\n')
+            idaapi.msg('TrashJump: address not found\nerror: %s\n' % traceback.format_exc())
 
     def term(self):
         pass
